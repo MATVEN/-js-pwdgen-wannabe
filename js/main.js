443fcf6f11c3b5e022f50d0e6ae6bc18fb9c9dda
MATVEN <mattiavena2993@gmail.com>
2019-10-16 16:33:21 +0200

    stamp

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // ask name user
-var inputuser = prompt("Write your name here!");
+var inputUser = prompt("Write your name here!");
+console.log(inputUser);
 
 // set up gretting string
-var gretting = "Hi" + inputuser;
+var gretting = "Hi" + inputUser;
 
 // ask surname user
 var surname = prompt("Write your surname here!");
+console.log(surname)
 
 // ask favourite color
 var color = prompt("What's your favourite color?");
+console.log(color)
 
 // set up final output string
-var outputfin =  "Password:" + inputuser + surname + color + "19";
+var outputFin =  "Password:" + inputUser + surname + color + "19";
+console.log(outputFin)
+
+document.getElementById('password').innerHTML=inputFinal;
