55c2342543435ed1d220c63a5f6c0d3a32a08111
MATVEN <mattiavena2993@gmail.com>
2019-10-16 16:08:02 +0200

    add input color e final string

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ var inputuser = prompt("Write your name here!");
 var gretting = "Hi" + inputuser;
 
 // ask surname user
-var inputuser = prompt("Write your surname here!");
+var surname = prompt("Write your surname here!");
+
+// ask favourite color
+var color = prompt("What's your favourite color?");
+
+// set up final output string
+var outputfin =  "Password:" + inputuser + surname + color + "19";
